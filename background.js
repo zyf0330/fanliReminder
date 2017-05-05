4f68299d27d428638bbe627f625139cbaf42ee8e
zyf <zyf034100@gmail.com>
2017-05-05 14:46:35 +0800

    finish base

---
+++ background.js
@@ -0,0 +1,19 @@
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+	if(changeInfo.url){
+		chrome.tabs.executeScript({
+			code: `
+					var href = "` + changeInfo.url + `";
+					var tarUrls = [
+						'buy.tmall.com/order/confirm_order.htm',
+						'buy.taobao.com/auction',
+						'www.amazon.cn/gp/buy/(addressselect|payselect|spc)/handlers/display',
+						'trade.jd.\w+/shopping/order/get(Easy)?OrderInfo'
+					]
+					var isTar = tarUrls.find(url => href.match(url)) != null;
+					if(isTar){
+						alert('别忘了使用返利网')
+					}
+					`
+		});
+	}
+})
