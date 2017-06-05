a945b64a44f5f9cb71d1f9422f8473b96eed0243
zhuisui <zyf034100@gmail.com>
2017-06-05 20:54:04 +0800

    修复地址匹配中的正则表达式无法匹配问题

--- background.js
+++ background.js
@@ -1,19 +1,20 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 	if(changeInfo.url){
 		chrome.tabs.executeScript({
-			code: `
-					var href = "` + changeInfo.url + `";
+			code: String.raw
+				`
+					var href = "${changeInfo.url}";
 					var tarUrls = [
 						'buy.tmall.com/order/confirm_order.htm',
 						'buy.taobao.com/auction',
 						'www.amazon.cn/gp/buy/(addressselect|payselect|spc)/handlers/display',
-						'trade.jd.\w+/shopping/order/get(Easy)?OrderInfo'
+						'trade.jd.\\w+/shopping/order/get(Easy)?OrderInfo'
 					]
-					var isTar = tarUrls.find(url => href.match(url)) != null;
+					var isTar = tarUrls.find(url => href.match(new RegExp(url))) != null;
 					if(isTar){
 						alert('别忘了使用返利网')
 					}
-					`
+				`
 		});
 	}
 })
